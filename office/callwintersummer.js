80c674ac6fe86f9490a92618e562db293209989f
ranjith <37235948+ranjithvbr@users.noreply.github.com>
2019-05-20 03:41:46 +0530

    Add files via upload

---
+++ office/callwintersummer.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  console.log(q);
+  var filename = "." + q.pathname;
+  console.log(filename);
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
